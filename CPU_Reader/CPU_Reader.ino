6228a55e4643ecabe2040218648cf0633fc8ea57
Kahngjoon Koh <46638829+kahngjoonkoh@users.noreply.github.com>
2022-07-26 22:53:46 +0400

    Create CPU_Reader.ino

---
+++ CPU_Reader/CPU_Reader.ino
@@ -0,0 +1,45 @@
+const char ADDR[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 2, 2, 2, 2};
+const char DATA[] = {0, 0, 0, 0, 0, 0, 0, 0};
+#define CLOCK 2
+#define READ_WRITE 3
+
+void setup() {
+  for (int n = 0; n < 16; n += 1) {
+    pinMode(ADDR[n], INPUT);
+  }
+  for (int n = 0; n < 8; n += 1) {
+    pinMode(DATA[n], INPUT);
+  }
+  pinMode(CLOCK, INPUT);
+  pinMode(READ_WRITE, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
+  
+  Serial.begin(57600);
+}
+
+void onClock() {
+  char output[15];
+
+  unsigned int address = 0;
+  for (int n = 0; n < 16; n += 1) {
+    int bit = digitalRead(ADDR[n]) ? 1 : 0;
+    Serial.print(bit);
+    address = (address << 1) + bit;
+  }
+  
+  Serial.print("   ");
+  
+  unsigned int data = 0;
+  for (int n = 0; n < 8; n += 1) {
+    int bit = digitalRead(DATA[n]) ? 1 : 0;
+    Serial.print(bit);
+    data = (data << 1) + bit;
+  }
+
+  sprintf(output, "   %04x  %c %02x", address, digitalRead(READ_WRITE) ? 'r' : 'W', data);
+  Serial.println(output);  
+}
+
+void loop() {
+}
